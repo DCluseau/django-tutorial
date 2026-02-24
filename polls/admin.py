6df6f4d83d930d8209c4428249622e411d13c978
DCluseau <daphne.cluseau@fms-ea.com>
2026-02-24 11:56:38 +0100

    Added display of options in admin part of website (display, filter, ordering, search)

--- polls/admin.py
+++ polls/admin.py
@@ -3,6 +3,19 @@ from django.contrib import admin
 from .models import Question
 from .models import Choice
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ["question_text", "pub_date"]
+    list_filter = ["question_text", "pub_date"]
+    ordering = ["-pub_date", "question_text"]
+    search_fields = ["question_text", "pub_date"]
+    pass
+admin.site.register(Question, QuestionAdmin)
 
-admin.site.register(Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ["question", "choice_text", "votes"]
+    list_filter = ["question", "choice_text", "votes"]
+    ordering = ["question", "choice_text", "votes"]
+    search_fields = ["choice_text", "votes", "foreign_key__related_fieldname"]
+    pass
+admin.site.register(Choice, ChoiceAdmin)
+
