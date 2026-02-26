823a1a881f0bda85a91df59de70e7896f49ef0b4
DCluseau <daphne.cluseau@fms-ea.com>
2026-02-26 13:18:32 +0100

    Updated code for statistics and all with exercise correction

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,12 @@
 from django.db.models import F
-from django.db.models.aggregates import Count, Sum
+from django.db.models import Count, Sum, Max
 from django.shortcuts import render, get_object_or_404
 
 from django.http import HttpResponse, Http404, HttpResponseRedirect, HttpRequest
 from django.urls import reverse
 from django.views import generic
+
+from .utils import minimax
 
 from .models import Question, Choice
 
@@ -24,11 +26,11 @@ class IndexView(generic.ListView):
 
 class AllView(generic.ListView):
     template_name = "all.html"
-    context_object_name = "latest_question_list"
+    model = Question
 
     def get_queryset(self):
         """Return all published questions."""
-        return Question.objects.order_by("question_text")
+        return Question.objects.order_by("id")
 
 class DetailView(generic.DetailView):
     model = Question
@@ -38,19 +40,19 @@ class FrequencyView(generic.DetailView):
     model = Question
     template_name = "frequency.html"
 
-class StatisticsView(generic.ListView):
-    template_name = "statistics.html"
-    context_object_name = "question_list"
-
-    def get_queryset(self):
-        """Return all published questions."""
-        return Question.objects.order_by("question_text")
-
-    @staticmethod
-    def get_total_nb_of_choices():
-        model = Question
-        count = 0
-        return count
+# class StatisticsView(generic.ListView):
+#     template_name = "statistics.html"
+#     context_object_name = "question_list"
+#
+#     def get_queryset(self):
+#         """Return all published questions."""
+#         return Question.objects.order_by("question_text")
+#
+#     @staticmethod
+#     def get_total_nb_of_choices():
+#         model = Question
+#         count = 0
+#         return count
 
 class ResultsView(generic.DetailView):
     model = Question
@@ -77,3 +79,25 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+
+def statistics(request):
+    nbr_sondages = Question.objects.count()
+    nbr_choix = Choice.objects.count()
+    nbr_votes = \
+        Choice.objects.aggregate(somme=Sum('votes'))['somme']
+    moyenne_par_sondage = nbr_votes / nbr_sondages
+    (question_la_moins_populaire, question_la_plus_populaire) = \
+        minimax()
+    id_derniere_question = \
+        Question.objects.aggregate(max=Max('id'))['max']
+    derniere_question = \
+        Question.objects.get(pk=id_derniere_question)
+    return render(request, 'statistics.html', {
+    'nbr_sondages': nbr_sondages,
+    'nbr_choix': nbr_choix,
+    'nbr_votes': nbr_votes,
+    'moyenne_par_sondage': moyenne_par_sondage,
+    'question_la_plus_populaire': question_la_plus_populaire,
+    'question_la_moins_populaire': question_la_moins_populaire,
+    'derniere_question': derniere_question,
+    })
