a5a9d22539bd1dd1618eb1d424d7e2afc7e1080f
DCluseau <daphne.cluseau@fms-ea.com>
2026-03-02 10:08:11 +0100

    customized admin page

--- polls/admin.py
+++ polls/admin.py
@@ -15,16 +15,5 @@ class QuestionAdmin(admin.ModelAdmin):
     list_display = ["question_text", "pub_date", "was_published_recently"]
     list_filter = ["pub_date"]
     search_fields = ["question_text"]
-    
+
 admin.site.register(Question, QuestionAdmin)
-
-
-# class ChoiceAdmin(admin.ModelAdmin):
-#     list_display = ["question", "choice_text", "votes"]
-#     list_filter = ["question", "choice_text", "votes"]
-#     ordering = ["question", "choice_text", "votes"]
-#     search_fields = ["choice_text", "votes", "foreign_key__related_fieldname"]
-#     pass
-# admin.site.register(Choice, ChoiceAdmin)
-
-# admin.site.register(Choice)
